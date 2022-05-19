cc6cecc4a53b0d5fbed8c5d35c581f28ef2f3769
Dylan <1975131479@qq.com>
2022-05-19 13:04:00 +0800

    hot-fix

--- nowcoder/src/OneToFifty/OneToTwenty.java
+++ nowcoder/src/OneToFifty/OneToTwenty.java
@@ -18,6 +18,7 @@ public class OneToTwenty {
         //    count++;
         //}
         //根据不同情况选择不同的算法
+        //hot-fix
         long start1 = System.currentTimeMillis();
         System.out.println(getcm2(7,5));
         long end1 = System.currentTimeMillis();
