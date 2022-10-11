3b193038b310834a6dfef7779a05092d15271bfb
DylanToT99 <105331051+DylanToT99@users.noreply.github.com>
2022-10-11 20:21:01 +0800

    Update four.java
    1到20

--- LeetCode/src/OneToTwenty/four.java
+++ LeetCode/src/OneToTwenty/four.java
@@ -6,7 +6,7 @@ import java.util.*;
  * @author Dylan
  * @version 1.0
  * @date 2022/5/22 15:18
- * @description TODO
+ * @description 
  **/
 public class four {
     public static void main(String[] args) {
